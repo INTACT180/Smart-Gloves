2e767ea4b9c0d021f4de62088735c63cecbcb686
Luis Jimenez <luis.gamarra26@gmail.com>
2018-03-25 00:17:37 -0400

    Added functionality to create a workout and also be able to view it on the edit workouts screen.

---
+++ unity-main/Assets/_Scripts/EditWorkouts.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class EditWorkouts : MonoBehaviour {
+
+	// These fields are added from the inspector in Unity.
+	[SerializeField] Transform menuPanel;
+	[SerializeField] GameObject buttonPrefab;
+
+	public Hashtable listOfWorkouts;
+	public string message;
+
+	// Use this for initialization
+	void Start () {
+			
+		// Check if the file exists.
+		if (File.Exists (Application.persistentDataPath + "/workoutTable.dat")) {
+
+			// Open the file and retrieve the workout list.
+			BinaryFormatter binaryFormatter = new BinaryFormatter();
+			FileStream file = File.Open (Application.persistentDataPath + "/workoutTable.dat", FileMode.Open);
+			WorkoutList workoutHistory = (WorkoutList) binaryFormatter.Deserialize(file);
+			file.Close ();
+
+			// Iterates through all of the created workouts.
+			foreach (DictionaryEntry workout in workoutHistory.workoutTable) {
+				Debug.Log (workout.Key);
+
+				// Creates the button.
+				GameObject button = (GameObject)Instantiate (buttonPrefab);
+				button.GetComponentInChildren<Text> ().text = (string)workout.Key;
+//				button.GetComponent<Button>().onClick.AddListener(
+//					
+//				)
+
+				button.transform.parent = menuPanel;
+			
+			}
+
+		} 
+	}
+		
+}
